f8be6a84018d157678f8a7c34b1029c543e30eeb
Srdjan Petkovic <srdjan.86@gmail.com>
2020-06-17 12:31:05 +0200

    Fix map icon possition

--- lib/ui/app/home/pp_map/pp_map.dart
+++ lib/ui/app/home/pp_map/pp_map.dart
@@ -9,27 +9,28 @@ class PPMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     PopupController _popupController = PopupController();
-    return new FlutterMap(
-      options: new MapOptions(
+    return FlutterMap(
+      options: MapOptions(
         plugins: [PopupMarkerPlugin()],
         onTap: (_) => _popupController.hidePopup(),
-        center: new LatLng(44.7866, 20.4489),
+        center: LatLng(44.7866, 20.4489),
         // interactive: true,
         maxZoom: 18,
         zoom: 12.0,
       ),
       layers: [
-        new TileLayerOptions(
+        TileLayerOptions(
             urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
             subdomains: ['a', 'b', 'c']),
-        new MarkerLayerOptions(
+        MarkerLayerOptions(
           markers: [
-            new Marker(
-              // width: 80.0,
-              // height: 80.0,
-              point: new LatLng(44.828190, 20.415110),
-              builder: (ctx) => new Container(
-                child: new Image.asset('assets/images/residence-512.png'),
+            Marker(
+              anchorPos: AnchorPos.align(AnchorAlign.top),
+              point: LatLng(44.828190, 20.415110),
+              builder: (ctx) => Image.asset(
+                'assets/images/residence-512.png',
+                height: 10,
+                width: 10,
               ),
             ),
           ],
@@ -37,35 +38,4 @@ class PPMap extends StatelessWidget {
       ],
     );
   }
-  // Widget build(BuildContext context) {
-  //   return new FlutterMap(
-  //     options: new MapOptions(
-  //       // center: new LatLng(51.5, -0.09),
-  //       zoom: 13.0,
-  //     ),
-  //     layers: [
-  //       new TileLayerOptions(
-  //         urlTemplate: "https://api.mapbox.com/v4/"
-  //             "{id}/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoidmFuaWxpY2EiLCJhIjoiY2tiYzkyZjh0MDhrczJxbjB5anEwbGI5byJ9.kojkghBtVvXPBiK7c3UYMQ",
-  //         additionalOptions: {
-  //           'accessToken':
-  //               'pk.eyJ1IjoidmFuaWxpY2EiLCJhIjoiY2tiYzkyZjh0MDhrczJxbjB5anEwbGI5byJ9.kojkghBtVvXPBiK7c3UYMQ',
-  //           'id': 'mapbox.streets',
-  //         },
-  //       ),
-  //       new MarkerLayerOptions(
-  //         markers: [
-  //           new Marker(
-  //             width: 80.0,
-  //             height: 80.0,
-  //             // point: new LatLng(51.5, -0.09),
-  //             builder: (ctx) => new Container(
-  //               child: new FlutterLogo(),
-  //             ),
-  //           ),
-  //         ],
-  //       ),
-  //     ],
-  //   );
-  // }
 }
